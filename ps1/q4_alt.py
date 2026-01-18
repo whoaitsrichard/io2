fd567edcfd38b1dccee58c27cd0edd4f31311aeb
richard-chen169 <richard.chen169@gmail.com>
2026-01-18 17:53:10 -0600

    Have something running but not working in 4_alt.py

--- ps1/q4_alt.py
+++ ps1/q4_alt.py
@@ -7,11 +7,9 @@ import numpy as np
 import pandas as pd
 import statsmodels.api as sm
 from statsmodels.sandbox.regression.gmm import IV2SLS
+from scipy.optimize import minimize
 
 df = pd.read_csv('data/ps1_ex4.csv')
-
-# u_{ijt} = x'_{jt} * beta + xi_{jt} + x_{jt} * gamma * nu_{i} + e_{ijt}
-# x_jt is vector of observed product characteristics with price
 
 num_i = 10000 # 1000 simulated consumers
 outer_tol = 1e-6
@@ -22,10 +20,8 @@ v_i = np.random.normal(0.0, 1.0, size=(num_i, 2))
 z_inst = np.array(df[['z1','z2','z3','z4','z5','z6']].values)
 z_inst_t = z_inst.T
 obs_shares = np.array(df.shares.values)
-opt_iv_mx_init = np.linalg.inv(z_inst_t @ z_inst)
+W = np.linalg.inv(z_inst_t @ z_inst)
 x_jt = df[['p','x']].values
-
-
 
 '''
 Done.
@@ -37,18 +33,11 @@ def calc_gmm_obj(temp_xi, temp_W):
     return gmm_temp
 
 '''
-Calculate gradient of gamma function
-'''
-def calc_gamma_grad():    
-    return
-
-'''
 Calculates the predicted market shares
 '''
 def calc_s_pred(temp_delta, temp_gamma):
     temp_gamma = np.insert(temp_gamma,1,0)
     temp_gamma = np.reshape(temp_gamma,(2,2))
-    temp_delta = np.random.rand(600)
 
     # temp_delta is 600x1, 
     temp_delta_proj = np.broadcast_to(temp_delta, (num_i,) + temp_delta.shape)
@@ -75,23 +64,52 @@ def calc_s_pred(temp_delta, temp_gamma):
 gamma = np.array([0.1, 0.1, 0.1]) #gamma_{11}, gamma_{21}, gamma_{22} respectively
 beta = [0.5, 0.5] # price coefficient and x coefficient respectively
 delta = np.random.rand(600)
-cm_obj = 1
 
-# Calculate init optimal IV matrix
-W = opt_iv_mx_init
-# Calculate initial guess for xi_j and compute opt_W
-W_opt = W
 
-# While Moment condition xi_res is greater than some tolerance
-gmm_obj = 1
-lr = 1e-6
-while gmm_obj > outer_tol:
-    # Update non-linear parameters: gammas
-    gamma -= gamma_jacobian * lr # gamma = gamma - jacobian*lr
-
+'''
+'''
+def calc_opt_W(temp_gamma, delta0):
+    delta=delta0.copy()
+    cm_obj = 1
     # Contraction mapping for delta - the mean utility levels for products
     while cm_obj > inner_tol:
-        s_pred = calc_s_pred(delta, gamma)
+        s_pred = calc_s_pred(delta, temp_gamma)
+        delta_k1 = delta + (np.log(obs_shares) - np.log(s_pred))
+        cm_obj = np.sum(np.abs(delta_k1 - delta))
+        delta = delta_k1
+
+    # Now we have the "correct" delta. 
+    reg_df = df.copy()
+    reg_df['y'] = delta
+    # Run 2SLS to estimate beta
+    # Use z as an instrument for Prices and run IV regression
+    controls = ['x']
+    # Regressors: const + endogenous + controls
+    exog = sm.add_constant(reg_df[['p'] + controls])
+
+    # Instruments: const + excluded instrument(s) + controls
+    instr = sm.add_constant(reg_df[['z1','z2','z3','z4','z5','z6'] + controls])
+    iv_model = IV2SLS(reg_df['y'], exog, instr).fit()
+    beta = iv_model.params.values[1:]
+
+    # Estimate xi, unobserved product heterogeneity
+    # xi = delta - np.mult(reg_df[['p','x']].values, beta)
+    xi = delta - reg_df[['p','x']].values @ beta
+
+    # Calculate moment res which is something with the instruments and the 
+    gmm_obj = calc_gmm_obj(xi, W)
+    print(gmm_obj)
+    return gmm_obj
+
+
+
+# Runs one loop of BLP
+def blp_obj(temp_gamma):
+
+    cm_obj = 1
+    # Contraction mapping for delta - the mean utility levels for products
+    while cm_obj > inner_tol:
+        s_pred = calc_s_pred(delta, temp_gamma)
         delta_k1 = delta + (np.log(obs_shares) - np.log(s_pred))
         cm_obj = np.sum(np.abs(delta_k1 - delta))
         delta = delta_k1
@@ -117,8 +135,26 @@ while gmm_obj > outer_tol:
     # Calculate moment res which is something with the instruments and the 
     gmm_obj = calc_gmm_obj(xi, W_opt)
 
-    # Calculate jacobian of gamma
-    gamma_jacobian = calc_gamma_grad()
+    return gmm_obj
+
+
+
+# Calculate init optimal IV matrix
+# Use Nelder-Mead
+
+res = minimize(
+    calc_opt_W,
+    x0=[0.1,0.1,0.1],
+    args=(delta,),                 # or args=(data1, data2, ...)
+    method="L-BFGS-B",
+    options={
+        "maxiter": 10_000,
+        "xatol": 1e-8,        # tolerance on x
+        "fatol": 1e-8,        # tolerance on f
+        "disp": True
+    }
+)
+
 
 
 
