412767244d601c29d7b6f081cdc5833f33ce612d
Pryce <ptdavies3@gmail.com>
2026-01-19 19:03:06 -0600

    Update q3_5.py

--- ps1/q3_5.py
+++ ps1/q3_5.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import random as rd
 import statsmodels.api as sm
+import statsmodels.formula.api as smf
 from linearmodels.iv import IV2SLS
 
 # Steps:
@@ -9,7 +10,7 @@ from linearmodels.iv import IV2SLS
 ## Draw x_{j}, \xi_{j} randomly, compute delta_{j} using \beta = 1
 ## compute exp(\rho^{-1}\delta_{j}), use to compute s_j, s_0 s_{j|G}
 ## Regress \ln(s_j)-\ln(s_0) on x_j, s_{j|G} instrumented w/ num. products
-rd.seed(2987)
+np.random.seed(2987)
 
 # Create Simulated Data 
 groups = np.array(list(range(1,11)))
@@ -30,17 +31,21 @@ df['s_j'] = df['exp_xi'].values*np.power(df['group_exp'],-.5)/(1+sum(np.power(df
 
 s_0 = 1-sum(df['s_j'].values)
 
-df['s_j_g'] = df['exp_xi'].values/df['group_exp'].values
+df['ln_s_j_g'] = np.log(df['exp_xi'].values/df['group_exp'].values)
 
 df['ln_s_j_ln_s_0'] = np.log(df['s_j'].values)- np.log(s_0)
 
 df['num_prod'] = df.groupby('group').transform('size')
 
 ## Regress \ln(s_j)-\ln(s_0) on x_j, s_{j|G} instrumented w/ num. products
-formula = 'ln_s_j_ln_s_0 ~ 0 + x + [s_j_g ~ num_prod]'
+formula = 'ln_s_j_ln_s_0 ~ 1 + x + [ln_s_j_g ~ num_prod]'
 
 
-# Fit the model
+# Fit the IV model
 model = IV2SLS.from_formula(formula, df)
-results = model.fit()
+results_iv = model.fit()
+print(results_iv)
 
+# No IV OLS
+results_noiv = smf.ols(formula='ln_s_j_ln_s_0 ~ 1 + x + ln_s_j_g', data=df).fit()
+print(results_noiv.summary())
